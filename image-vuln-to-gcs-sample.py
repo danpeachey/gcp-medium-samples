3cf7ddeea133aca137621a4913e0225bf1a6a4c0
Dan Peachey <danpeachey@users.noreply.github.com>
2021-02-03 08:55:21 -0800

    Create image-vuln-to-gcs-sample.py
    Initial commit

---
+++ image-vuln-to-gcs-sample.py
@@ -0,0 +1,27 @@
+import base64
+import os
+import json
+from google.cloud.devtools import containeranalysis_v1
+from google.cloud import storage
+
+def write_vuln_to_bucket(bucket_name, vuln_text, destination_object_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    obj = bucket.blob(destination_object_name)
+    obj.upload_from_string(vuln_text)
+
+def image_vuln_pubsub_handler(event, context):
+    #get the Pub/Sub message containing the vulnerability occurrence id
+    data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+
+    #load in environment variables for GCS bucket.  
+    bucket_name = os.environ.get("BUCKET_NAME", "Bucket name not set")
+
+    #get the occurrence via the grafeas client
+    occurrence_name = (data['name'])
+    client = containeranalysis_v1.ContainerAnalysisClient()
+    grafeas_client = client.get_grafeas_client()
+    o = grafeas_client.get_occurrence(name=occurrence_name)
+
+    #write to storage
+    write_vuln_to_bucket(bucket_name, str(o), str(o.name))
